e40b59366cea9a6035e33bed46daa4de8f974975
IT21182396 <99359954+IT21182396@users.noreply.github.com>
2023-05-17 15:09:41 +0530

    Das

--- backend/models/CategoryReportDecument.js
+++ backend/models/CategoryReportDecument.js
@@ -1,8 +1,13 @@
+
+
 module.exports=(categories)=>{
 
+
   
-  const today=new Date()
-  
+  const today=new Date() 
+
+
+
   return `
   
   <!doctype html>
@@ -69,7 +74,7 @@ module.exports=(categories)=>{
     
     #project span {
       color: #5D6975;
-      text-align: right;
+      text-align: left;
       width: 52px;
       margin-right: 10px;
       display: inline-block;
@@ -150,6 +155,9 @@ module.exports=(categories)=>{
       padding: 8px 0;
       text-align: center;
     }
+    teH{
+      font-size:3.5em;
+    }
   
   </style>
 
@@ -158,25 +166,23 @@ module.exports=(categories)=>{
   <body>
 
   <header class="clearfix">
-  <div id="logo">
-    <img src=".../frontend/src/components/Layouts/Images/Onex_logo.png">
-  </div>
-  <h1><b>ONEX Bookshop (PVT)</b></h1>
-  <h1>Inventory Details</h1>
 
-  <div id="company" class="clearfix">
-    <div>ONEX Bookshop PVT LMT</div>
-    <div>Walivita Road<br /> No 12,Waliwita Malabe</div>
-    <div>0272226088</div>
-    <div><a href="mailto:company@example.com">onex_book4u@gmail.com</a></div>
+  <div id="logo">
+
+  <img src="./logo.png">
   </div>
+
+  <h1>Inventory Details</h1>
+ 
+
   <div id="project">
-    <div><span>Position</span>Stock Controller</div>
-    <div><span>Name</span>Asanka Ranaweera</div>
    
-    <div><span>EMAIL</span> <a href="mailto:john@example.com">asankapranaweera@gmail.com</a></div>
-    <div><span>DATE</span>${`${today}`}</div>
-   
+  
+    <h2>ONEX BookShop</h2>
+    <h3><span>Position :</span>Stock Controller</h3>
+    <h3><span>Email  : </span><a href="mailto:john@example.com">info@ONEXbookshop.com</a></h3>
+    <h3><span>Date  : </span>${`${today}`}</h3>
+
   </div>
 </header>
 <main>
